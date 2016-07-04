2453b8cfd922dff844405cd14e8089ca59e157d5
TILE <nguyenngoi9190@gmail.com>
2016-07-04 18:25:29 +0700

    update port

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-const express = require('express');
-const app = express();
+// var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
+app.set('port', (process.env.PORT || 1337));
+// app.use(express.statuc(__dirname + '/public'));
+
+// views is directory for all template files
+// app.set('views', __dirname + '/views');
+// app.set('view angine', 'ejs');
 
 app.get('/', function (req, res) {
 	res.send('hello word!');
 });
 
-app.get('/webhook', function(req, res) {
+app.post('/webhook', function(req, res) {
   res.send('hello world!');
+});
+
+app.listen(app.get('port'), function () {
+	console.log('Node app is running on port', app.get('port'));
 });
