17a19aaee7ce20232f12b3795dbedc047d1f6a68
TILE <nguyenngoi9190@gmail.com>
2016-07-04 18:40:25 +0700

    update webhook

--- index.js
+++ index.js
@@ -13,8 +13,15 @@ app.get('/', function (req, res) {
 	res.send('hello word!');
 });
 
-app.post('/webhook', function(req, res) {
-  res.send('hello world!');
+app.get('/webhook', function(req, res) {
+   if (req.query['hub.mode'] === 'subscribe' &amp;&amp;
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
 });
 
 app.listen(app.get('port'), function () {
